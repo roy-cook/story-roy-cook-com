c051b646a43f67b74c5de7591fd14fba4b7eddcd
RoyLH <liuheng@yidian-inc.com>
2019-05-07 19:50:24 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+pwd
+ls
+# docker stop story-roy-cook-com
+# docker rm story-roy-cook-com
+docker rmi roy-cook/story-roy-cook-com
+docker build -t roy-cook/story-roy-cook-com .
+docker run -d --name story-roy-cook-com -p 4000:4000 roy-cook/story-roy-cook-com
